20ddd80f89b20ca94a0a23f0e1b7dd56d43a35ba
hhwang <wang@ispan.com.tw>
2022-04-13 17:14:13 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyGitAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
